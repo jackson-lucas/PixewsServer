cb6666898446dd6e21e629c0974d86992e567e01
Jackson Lucas <jackson7br@gmail.com>
2016-09-19 21:20:01 -0400

    refactor : track query

--- routes/fotografo.js
+++ routes/fotografo.js
@@ -44,10 +44,12 @@ const getImagens = {
   method: 'GET',
   path: '/fotografo/imagens',
   handler: function (request, reply) {
+    var chave = request.query.chave.replace(/-/g,'\\-')
+    debug(chave)
     if(TokenGenerator.isValid(request.headers.token)) {
-      request.query.chave = request.query.chave.replace(/-/g,'\\-') 
       // http://ec2-54-197-15-18.compute-1.amazonaws.com:8983/solr/gettingstarted/select?wt=json&indent=true&q=fotografo_id:12
-      reqwest(`http://localhost:8983/solr/pixews/select?wt=json&indent=true&q=fotografo_id:${request.query.chave}`,
+      debug(`http://localhost:8983/solr/pixews/select?wt=json&indent=true&q=fotografo_id:${chave}`)
+      reqwest(`http://localhost:8983/solr/pixews/select?wt=json&indent=true&q=fotografo_id:${chave}`,
       function (error, response, body) {
 
         if (!error && response.statusCode == 200) {
